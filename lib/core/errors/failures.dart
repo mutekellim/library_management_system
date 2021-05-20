42e395d39694e325a9cc5823bf0c051a16f59a9d
yusufdag <yusuf@baseflow.com>
2021-05-20 16:17:31 +0200

    Add failures class

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+/// [Failure] class is an abstract class for failure
+/// implementations.
+abstract class Failure extends Equatable {
+  const Failure();
+
+  @override
+  List<Object> get props => [];
+}
