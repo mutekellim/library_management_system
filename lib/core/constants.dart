5b127ec68a7adbc11f10961ba9ffefe059b5da65
yusufdag <yusuf@baseflow.com>
2021-05-22 16:39:30 +0200

    Add constants class

---
+++ lib/core/constants.dart
@@ -0,0 +1,23 @@
+/// Inventory types
+
+const String INVENTORY_TYPE_BOOK = 'Book';
+
+const String INVENTORY_TYPE_JOURNAL = 'Journal';
+
+const String INVENTORY_TYPE_DVD = 'Dvd';
+
+/// Inventory status
+
+const String INVENTORY_STATUS_AVAILABLE = 'Available';
+
+const String INVENTORY_STATUS_RESERVED = 'Reserved';
+
+const String INVENTORY_STATUS_LOANED = 'Loaned';
+
+const String INVENTORY_STATUS_LOST = 'Lost';
+
+/// Book types
+
+const String BOOK_TYPE_TEXTBOOK = "Textbook";
+
+const String BOOK_TYPE_OTHERS = "Others";
