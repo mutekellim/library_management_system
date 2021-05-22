5cfa5e599f92cbb4d64024e9a18587364cf8db99
yusufdag <yusuf@baseflow.com>
2021-05-22 23:06:59 +0200

    Add book repository implementation

--- lib/data/repositories/book_repository_impl.dart
+++ lib/data/repositories/book_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:dartz/dartz.dart';
 import '../../core/errors/failures.dart';
 import '../../domain/entities/entities.dart';
 import '../../domain/repositories/repositories.dart';
+import '../sources/dao/dao.dart';
 
 /// [BookRepositoryImpl] class is an implementation of [BookRepository] and
 /// implements [InventoryRepository] interface as well.
@@ -12,27 +13,50 @@ import '../../domain/repositories/repositories.dart';
 ///
 /// Returns [DatabaseFailure] when an error occurred.
 class BookRepositoryImpl implements BookRepository, InventoryRepository {
+
+  final BookModelDao localDataSource;
+
+  BookRepositoryImpl({
+    required this.localDataSource,
+  });
+
   @override
-  Future<Either<Failure, List<Book>>> searchByPubDate(String publishDate) {
-    // TODO: implement searchByPubDate
-    throw UnimplementedError();
+  Future<Either<Failure, List<Book>>> searchByPubDate(String publishDate) async {
+    try {
+      final bookList = await localDataSource.searchByPubDate(publishDate);
+      return Right(bookList.map((bookModel) => Book.fromModel(bookModel)).toList());
+    } catch (_) {
+      return Left(DatabaseFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<Book>>> searchBySubject(String subject) {
-    // TODO: implement searchBySubject
-    throw UnimplementedError();
+  Future<Either<Failure, List<Book>>> searchBySubject(String subject) async {
+    try {
+      final bookList = await localDataSource.searchBySubject(subject);
+      return Right(bookList.map((bookModel) => Book.fromModel(bookModel)).toList());
+    } catch (_) {
+      return Left(DatabaseFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<Book>>> searchByTitle(String title) {
-    // TODO: implement searchByTitle
-    throw UnimplementedError();
+  Future<Either<Failure, List<Book>>> searchByTitle(String title) async {
+    try {
+      final bookList = await localDataSource.searchByTitle(title);
+      return Right(bookList.map((bookModel) => Book.fromModel(bookModel)).toList());
+    } catch (_) {
+      return Left(DatabaseFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<Book>>> searchByType(InventoryTypes type) {
-    // TODO: implement searchByType
-    throw UnimplementedError();
+  Future<Either<Failure, List<Book>>> searchByType(String type) async {
+    try {
+      final bookList = await localDataSource.searchByType(type);
+      return Right(bookList.map((bookModel) => Book.fromModel(bookModel)).toList());
+    } catch (_) {
+      return Left(DatabaseFailure());
+    }
   }
 }
