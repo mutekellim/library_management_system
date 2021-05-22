0904835d75a94418741f83eebf8c388918fa6922
yusufdag <yusuf@baseflow.com>
2021-05-22 16:46:33 +0200

    Add book and journal dao classes

---
+++ lib/data/sources/dao/book_model_dao.dart
@@ -0,0 +1,19 @@
+import 'package:floor/floor.dart';
+
+import '../../models/models.dart';
+
+@dao
+abstract class BookModelDao {
+
+  @Query('SELECT * FROM BookModel WHERE publishDate = :publishDate')
+  Future<List<BookModel>> searchByPubDate(String publishDate);
+
+  @Query('SELECT * FROM BookModel WHERE subject LIKE :subject')
+  Future<List<BookModel>> searchBySubject(String subject);
+
+  @Query('SELECT * FROM BookModel WHERE title LIKE :title')
+  Future<List<BookModel>> searchByTitle(String title);
+
+  @Query('SELECT * FROM BookModel WHERE type = :type')
+  Future<List<BookModel>> searchByType(String type);
+}
---
+++ lib/data/sources/dao/dao.dart
@@ -0,0 +1,2 @@
+export 'book_model_dao.dart';
+export 'journal_model_dao.dart';
