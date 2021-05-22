1bbd35a178f9943f87f4d5851c2cdb8f0011a11c
yusufdag <yusuf@baseflow.com>
2021-05-22 16:39:45 +0200

    Add journal model class

---
+++ lib/data/models/journal_model.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/foundation.dart';
+import 'package:equatable/equatable.dart';
+import 'package:floor/floor.dart';
+
+/// [BookModel] represent tha data provided by our data providers.
+///
+@entity
+class JournalModel extends Equatable {
+  @primaryKey
+  final int id;
+  final int typeId;
+
+  final String isbn;
+  final String title;
+  final String subject;
+  final String publisher;
+  final String language;
+  final String publishDate;
+  final String type;
+  final String status;
+  final String volume;
+  final String issue;
+
+
+  JournalModel({
+    required this.id,
+    required this.typeId,
+    required this.isbn,
+    required this.title,
+    required this.subject,
+    required this.publisher,
+    required this.language,
+    required this.publishDate,
+    required this.type,
+    required this.status,
+    required this.volume,
+    required this.issue,
+  });
+
+  @override
+  List<Object> get props => [
+    id,
+    typeId,
+    isbn,
+    title,
+    subject,
+    publisher,
+    language,
+    publishDate,
+    type,
+    status,
+    volume,
+    issue,
+  ];
+
+  @override
+  String toString() => 'BookModel {id: $id, typeId: $typeId, '
+      'isbn: $isbn, title:$title, subject: $subject, publisher: $publisher, '
+      'language: $language, publishDate:$publishDate, type:$type, status:$status, '
+      'bookType: $volume, authors: $issue}';
+}
---
+++ lib/data/models/models.dart
@@ -0,0 +1,2 @@
+export 'book_model.dart';
+export 'journal_model.dart';
