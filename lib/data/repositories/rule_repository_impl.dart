03fa44154f583e4151323903b3fc7d040d7d83e8
mutekellim <mutekellim@yahoo.com>
2021-05-28 15:34:52 +0300

    Set Rules

---
+++ lib/data/repositories/rule_repository_impl.dart
@@ -0,0 +1,43 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:dartz/dartz.dart';
+
+import '../sources/dao/dao.dart';
+import '../../core/errors/failures.dart';
+import '../../domain/entities/entities.dart';
+import '../../domain/repositories/rule_repository.dart';
+
+class RuleRepositoryImpl extends RuleRepository {
+  RuleModelDao localDataSource;
+
+  RuleRepositoryImpl({
+    required this.localDataSource,
+  });
+
+  Future<Either<Failure, Rule>> addRule(Rule rule) async {
+    try {
+      await localDataSource.addRule(rule.toModel());
+      return Right(rule);
+    } catch (_) {
+      return Left(DatabaseFailure());
+    }
+  }
+
+  Future<Either<Failure, Rule>> updateRule(Rule rule) async {
+    try {
+      await localDataSource.updateRule(rule.toModel());
+      return Right(rule);
+    } catch (_) {
+      return Left(DatabaseFailure());
+    }
+  }
+
+  Future<Either<Failure, Rule>> getRule(int ruleId) async {
+    try {
+      final rule = await localDataSource.getRule(ruleId);
+      return Right(Rule.fromModel(rule!));
+
+    } catch (_) {
+      return Left(DatabaseFailure());
+    }
+  }
+}
